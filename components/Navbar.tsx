a9e88f3eb57589620b2816844570d217a1879b81
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-09-15 17:28:46 +0900

    fix navbar-round

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <div className="navbar fixed z-50 bg-orange-100 rounded-xl">
+        <div className="navbar fixed z-50 bg-orange-100 rounded-r-xl">
             <div className="flex-none">
                 <button className="btn btn-square btn-ghost">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
